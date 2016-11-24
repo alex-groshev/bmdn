c7334599ce869537abc992f409ddf09a04f7eb5e
Alexey Groshev <aleksejs.grosevs@gmail.com>
2016-11-24 10:05:25 +0000

    List vs Queue LINQ.

--- BenchmarkDotNetSandbox/BenchmarkDotNetSandbox/Program.cs
+++ BenchmarkDotNetSandbox/BenchmarkDotNetSandbox/Program.cs
@@ -7,7 +7,7 @@ namespace BenchmarkDotNetSandbox
     {
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<StringReverse>();
+            var summary = BenchmarkRunner.Run<ListVsQueue>();
         }
     }
 }
---
+++ BenchmarkDotNetSandbox/BenchmarkDotNetSandbox/Tests/ListVsQueue.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+
+namespace BenchmarkDotNetSandbox.Tests
+{
+    [Config(typeof(Config))]
+    public class ListVsQueue
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(Job.LegacyJitX64);
+                Add(Job.RyuJitX64);
+                Add(MarkdownExporter.StackOverflow);
+            }
+        }
+
+        private readonly Queue<int> _queue = new Queue<int>(10);
+        private readonly List<int> _list = new List<int>(10);
+
+        [Setup]
+        public void Setup()
+        {
+            _queue.Enqueue(2);
+            _queue.Enqueue(5);
+            _queue.Enqueue(6);
+            _queue.Enqueue(3);
+            _queue.Enqueue(0);
+            _queue.Enqueue(7);
+            _queue.Enqueue(1);
+            _queue.Enqueue(8);
+            _queue.Enqueue(9);
+            _queue.Enqueue(4);
+
+            _list.Add(2);
+            _list.Add(5);
+            _list.Add(6);
+            _list.Add(3);
+            _list.Add(0);
+            _list.Add(7);
+            _list.Add(1);
+            _list.Add(8);
+            _list.Add(9);
+            _list.Add(4);
+        }
+
+        [Benchmark(Baseline = true)]
+        public int QueueLinq()
+        {
+            return _queue.Count(x => x > 5);
+        }
+
+        [Benchmark]
+        public int ListLinq()
+        {
+            return _list.Count(x => x > 5);
+        }
+    }
+}
