944a599d31ba9d5e581b2ed56e7409b0fa8dec17
Alexey Groshev <aleksejs.grosevs@gmail.com>
2016-12-22 09:48:25 +0000

    Null or empty.

--- BenchmarkDotNetSandbox/BenchmarkDotNetSandbox/Program.cs
+++ BenchmarkDotNetSandbox/BenchmarkDotNetSandbox/Program.cs
@@ -7,7 +7,7 @@ namespace BenchmarkDotNetSandbox
     {
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<IsPermutation>();
+            var summary = BenchmarkRunner.Run<NullOrEmpty>();
         }
     }
 }
---
+++ BenchmarkDotNetSandbox/BenchmarkDotNetSandbox/Tests/NullOrEmpty.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+
+namespace BenchmarkDotNetSandbox.Tests
+{
+    [Config(typeof(Config))]
+    public class NullOrEmpty
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(Job.LegacyJitX64);
+                Add(Job.RyuJitX64);
+                Add(MarkdownExporter.StackOverflow);
+            }
+        }
+
+        private List<int> _list = new List<int> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        [Benchmark(Baseline = true)]
+        public bool NullCountProperty()
+        {
+            return _list == null || _list.Count == 0;
+        }
+
+        [Benchmark]
+        public bool NullCountLinq()
+        {
+            return _list == null || _list.Count() == 0;
+        }
+
+        [Benchmark]
+        public bool NullAny()
+        {
+            return _list == null || !_list.Any();
+        }
+    }
+}
